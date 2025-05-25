406cda75f10e038d090deb75e09f24b2d5f08961
angel4C <alopezg64@miumg.edu.gt>
2025-05-25 16:00:56 -0600

    Update

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def login(username: str, password: str):
     log_login_attempt("N/A", username, "login_success" if success else "login_failed")
     if not success:
         raise HTTPException(status_code=401, detail="Credenciales inválidas.")
-    return {"msg": "Login exitoso"}
+    return {"msg": "ok"}
 
 
 @app.get("/")
