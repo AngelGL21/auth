1364140891e03980e80db1547e7d6b2061156842
AngelLG21 <alopezg4c@gmail.com>
2025-06-08 05:03:00 -0600

    Feat/Evaluacion de tecnologias

--- src/routers/router.py
+++ src/routers/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from controllers.auth import register, login, get_users, simulate_error, change_password, refresh_token
+from controllers.auth import register, login, get_users, simulate_error, change_password, refresh_token, evaluar_tecnologias
 from controllers.monitor import obtener_errores, obtener_causas, obtener_acciones
 
 router = APIRouter()
@@ -18,6 +18,11 @@ monitor_router.get("/causas")(obtener_causas)
 monitor_router.get("/acciones")(obtener_acciones)
 monitor_router.post("/simulate_error")(simulate_error)
 
+tecnologia_router = APIRouter(prefix="/tecnologia", tags=["Tecnología"])
+tecnologia_router.get("/evaluation")(evaluar_tecnologias)
+
+
 # Incluye los routers en el router principal
 router.include_router(auth_router)
 router.include_router(monitor_router)
+router.include_router(tecnologia_router)
